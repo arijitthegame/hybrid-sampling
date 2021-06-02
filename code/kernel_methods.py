db5994b5042194703d2fbe4f3c794a1c8ffdfd94
Arijit Sehanobish <arijitthegame@gmail.com>
2021-06-02 12:00:23 -0400

    Add FAVOR+ approximation

--- code/kernel_methods.py
+++ code/kernel_methods.py
@@ -46,3 +46,62 @@ class RFF():
         Z = self.transform(X)
         K = torch.matmul(Z,Z.t())
         return K
+
+
+def cosh(X):
+    return (torch.exp(X)+torch.exp(-X))/2.0
+
+class FavorPlus():
+    def __init__(self, gamma = 1, D = 50):
+        '''
+        Random positive  Features to approximate a Gaussian Kernel. 
+        D = output dimension of the features
+        gamma = temperature, where the kernel K(x-y) = exp(-gamma/2 ||x-y||^2)
+        '''
+        self.gamma = gamma
+        self.D = D
+        
+    
+
+    def generate(self, X):
+        """ Generates random samples from isotropic Gaussian """
+        d = X.shape[1]
+       
+        p = distributions.multivariate_normal.MultivariateNormal(torch.zeros(d), math.sqrt(self.gamma)*torch.eye(d)) 
+       
+        #Generate D iid samples from p(w) 
+        
+        w = p.sample(torch.Size([self.D]))
+        return w
+
+        #TODO : Create orthogonal features
+    
+    def transform(self,X):
+        """ Transforms the data X (n_samples, n_features) to the new map space Z(X) (n_samples, n_components)"""
+        #Compute feature map Z(x):
+        
+        w = self.generate(X)
+       
+        Z = math.sqrt(2/self.D)*torch.exp(torch.matmul(X,w.t()))*(torch.exp(-torch.linalg.norm(X, dim=1)**2)).view(-1,1)
+        return Z
+    
+    def compute_kernel(self, X):
+        """ Computes the approximated kernel matrix K """
+        
+        Z = self.transform(X)
+        K = torch.matmul(Z,Z.t())
+        return K
+
+    def approximate_softmax(self,X,Y):
+        """ Computes the approximate softmax kernel"""
+        #TODO Use orthogonal vectors instead of just random
+        Z = X + Y
+        X1, Y1 = self.transform(X), self.transform(Y)
+        Lambda = torch.exp((torch.linalg.norm(X,dim=1)**2 + torch.linalg.norm(Y,dim=1)**2)/2.0) 
+        approx = torch.mm(X1,Y1.t())*Lambda.view(-1,1)
+        return approx
+
+    #TODO: Bug somewhere since the computed matrices are far from stochastic
+
+
+
